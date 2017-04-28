116120ae5d01161400367a9acdfb5b7bab9d29a1
erenjun <913496866@qq.com>
2017-04-28 18:43:12 +0800

    fdsfgsdg

--- js/common.js
+++ js/common.js
@@ -25,6 +25,42 @@ var baseUserInfo = [
 			goodsName:"测试货单3",
 			createTime:"2017-04-28"
 		}
-	]
+	],
+	deliveryNotes = [//送货单，goodsId：货单id；goodsName：货单名；createTime：货单创建日期
+		{
+			goodsId:1,
+			goodsName:"送货单1",
+			createTime:"2017-04-28"
+		},
+		{
+			goodsId:2,
+			goodsName:"送货单2",
+			createTime:"2017-04-28"
+		},
+		{
+			goodsId:3,
+			goodsName:"送货单3",
+			createTime:"2017-04-28"
+		}
+	],
+	goodsReturnedNotes = [//退货单，goodsId：货单id；goodsName：货单名；createTime：货单创建日期
+		{
+			goodsId:1,
+			goodsName:"退货单1",
+			createTime:"2017-04-28"
+		},
+		{
+			goodsId:2,
+			goodsName:"退货单2",
+			createTime:"2017-04-28"
+		},
+		{
+			goodsId:3,
+			goodsName:"退货单3",
+			createTime:"2017-04-28"
+		}
+	];
 localStorage.baseUserInfo = JSON.stringify(baseUserInfo);
 localStorage.baseGoodsInfo = JSON.stringify(baseGoodsInfo);
+localStorage.deliveryNotes = JSON.stringify(deliveryNotes);
+localStorage.goodsReturnedNotes = JSON.stringify(goodsReturnedNotes);
---
+++ js/index.js
@@ -0,0 +1,20 @@
+(function($){
+	var main = {
+		
+		//初始化
+		init:function(){
+			var self = this;
+			self.bindEvent();
+		},
+		//数据绑定
+		bindEvent:function(){
+			
+		}
+		
+		
+	
+	}
+	
+	main.init();
+	
+})(mui)
